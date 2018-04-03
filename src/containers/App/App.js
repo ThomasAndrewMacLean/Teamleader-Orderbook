dfa7e22496c638c2a027189a4694ee938ebbc396
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-04-03 14:27:02 +0200

    List is a container, no longer simple component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import List from './../../components/List/List'
+import List from './../List/List'
 
 import './App.css';
 
