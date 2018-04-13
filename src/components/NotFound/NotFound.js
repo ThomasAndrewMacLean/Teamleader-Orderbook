257c38e5360dbf3eb561a939eaaffadf3d559c48
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-04-13 16:54:06 +0200

    styling 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './NotFound.css';
-const NotFound = () => {
+import PropTypes from 'prop-types';
 
-    return (
-        <div className="not-found">
-            <div className="">
-                404 page not found...
+
+export class NotFound extends Component {
+    goHome() {
+        this.props.history.push('/');
+    }
+    render() {
+        return (
+            <div className="not-found">
+                <div className="not-found-text-wrapper" onClick={() => this.goHome()}>
+                    <div> 404 page not found...  <i className="big-text">:(</i>
+                    </div>
+                    <br />
+                    <div className="smaller-text">
+                        Click here to go to order overview
+                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+}
+NotFound.propTypes = {
+
+
+    history: PropTypes.object,
+
 };
+
 
 export default NotFound;
